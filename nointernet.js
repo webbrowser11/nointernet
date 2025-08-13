27c9c0296e2176c4ba43c84e6decf284419f741c
T_cat9000_2 <terpstragraham@gmail.com>
2025-08-12 18:41:53 -0700

    pls

--- nointernet.js
+++ nointernet.js
@@ -1,30 +1,14 @@
 (function (Scratch) {
   "use strict";
 
-  class AirplaneModeBlocker {
-    constructor() {
-      this.airplaneModeOn = false;
-    }
-
+  class HTTPBlocker {
     getInfo() {
       return {
-        id: "airplaneModeBlocker",
-        name: "Airplane Mode Blocker",
-        color1: "#ff0000",
-        color2: "#aa0000",
+        id: "httpBlocker",
+        name: "HTTP Blocker",
+        color1: "#d62828",
+        color2: "#9e1b1b",
         blocks: [
-          {
-            opcode: "setAirplaneMode",
-            blockType: Scratch.BlockType.COMMAND,
-            text: "set airplane mode to [MODE]",
-            arguments: {
-              MODE: {
-                type: Scratch.ArgumentType.STRING,
-                menu: "onOff",
-                defaultValue: "on",
-              },
-            },
-          },
           {
             opcode: "httpRequest",
             blockType: Scratch.BlockType.REPORTER,
@@ -37,36 +21,14 @@
             },
           },
         ],
-        menus: {
-          onOff: {
-            acceptReporters: true,
-            items: [
-              { text: "on", value: "on" },
-              { text: "off", value: "off" },
-            ],
-          },
-        },
       };
     }
 
-    setAirplaneMode(args) {
-      this.airplaneModeOn = args.MODE === "on";
-    }
-
-    httpRequest(args) {
-      if (this.airplaneModeOn) {
-        // Block the request
-        return "Error: Network blocked by Airplane Mode";
-      }
-      // Allow request normally
-      return fetch(args.URL)
-        .then((response) => {
-          if (!response.ok) throw new Error("Network error");
-          return response.text();
-        })
-        .catch(() => "Error: Request failed");
+    httpRequest() {
+      // Block *every* HTTP request, always.
+      return "Error: Network blocked";
     }
   }
 
-  Scratch.extensions.register(new AirplaneModeBlocker());
+  Scratch.extensions.register(new HTTPBlocker());
 })(Scratch);
